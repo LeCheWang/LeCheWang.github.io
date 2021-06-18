aff060c51ee8442aea9b1668af70663c6d944f2f
HaVanPhong <haphonggoroki@gmail.com>
2021-06-19 01:47:21 +0700

    eval cal

---
+++ caculator/js.js
@@ -0,0 +1,48 @@
+function getOldResult() {
+    return document.getElementById("inp").innerText;
+}
+
+function setOldResult(so) {
+    document.getElementById("inp").innerText = so;
+}
+
+function getResult() {
+    return document.getElementById("outp").innerText;
+}
+
+function setResult(so) {
+    document.getElementById("outp").innerText = so;
+}
+var system = document.getElementsByClassName("system");
+
+for (const item of system) {
+    item.addEventListener("click", () => {
+        if (item.value === "AC") {
+            setResult(0);
+            setOldResult("");
+        } else if (item.value === "C") {
+            let t = getResult();
+            setResult(t.substring(0, t.length - 1));
+        } else if (item.value == "=") {
+            let k = eval(getResult());
+            setResult(k);
+            check = true;
+        }
+    })
+
+}
+var check;
+const inpNumber = document.getElementsByClassName("so");
+for (const value of inpNumber) {
+    value.addEventListener("click", () => {
+        if (check) {
+            setResult("0")
+        }
+        var result = getResult() === "0" ? "" : getResult();
+        if (result != NaN) {
+            result += value.value;
+            setResult(result);
+            check = false;
+        }
+    })
+}
